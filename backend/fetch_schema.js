34708555e140e220d035c149d7d68f13cb91af11
stuti-006 <nagastuti.06@gmail.com>
2026-04-15 21:57:15 +0530

    Added backend folder

---
+++ backend/fetch_schema.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const http = require('https');
+
+const options = {
+  hostname: 'wdrolrtyceaohpjinnyt.supabase.co',
+  path: '/rest/v1/',
+  method: 'GET',
+  headers: {
+    'apikey': SUPABASE_ANON_KEY
+  }
+};
+
+const req = http.request(options, (res) => {
+  let data = '';
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    require('fs').writeFileSync('schema.json', data);
+    console.log('Schema saved to schema.json');
+  });
+});
+
+req.on('error', (error) => {
+  console.error(error);
+});
+
+req.end();
